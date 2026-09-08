36bcbcd7223b1c67a824b91f63922eae7f6c0817
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-09-08 23:20:02 +0600

    feat: add global exception handler for task errors

---
+++ src/main/java/com/example/task_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.example.task_management.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            TaskNotFoundException exception){
+        
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(exception.getMessage()));
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException exception) {
+        
+        String message = exception.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+        
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(message));
+    }
+    
+}
